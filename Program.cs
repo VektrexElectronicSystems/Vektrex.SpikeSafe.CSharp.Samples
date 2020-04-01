37bb0b37890d4c9b20e81b2149954d09006fe3b6
eljayg <58857996+eljayg@users.noreply.github.com>
2020-03-31 17:41:39 -0700

    Finishing project setup on master

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpikeSafeCSharpExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
