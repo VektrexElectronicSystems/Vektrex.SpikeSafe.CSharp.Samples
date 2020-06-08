fcfb3bfd8bfcae560112228abbe0e7bf93efb951
Eljay <eljayg@gmail.com>
2020-06-08 15:19:30 -0700

    Added user menu implementation and TcpSample layout

---
+++ src/SpikeSafeCSharpSamples/GettingStarted/TcpSample.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SpikeSafeCSharpSamples.GettingStarted
+{
+    public class TcpSample
+    {
+        public void Run()
+        {
+            Console.WriteLine("TcpSample run()");
+        }
+    }
+}
