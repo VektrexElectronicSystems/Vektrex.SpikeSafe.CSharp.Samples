47285b4edfd3cd31556bf1bce724b3a8b444aebb
Eljay <eljayg@gmail.com>
2020-06-08 16:22:29 -0700

    Added NLog foundation

--- src/SpikeSafeCSharpSamples/GettingStarted/TcpSample.cs
+++ src/SpikeSafeCSharpSamples/GettingStarted/TcpSample.cs
@@ -4,9 +4,12 @@ namespace SpikeSafeCSharpSamples.GettingStarted
 {
     public class TcpSample
     {
+        private static NLog.Logger _log = NLog.LogManager.GetCurrentClassLogger();
+
         public void Run()
         {
             Console.WriteLine("TcpSample run()");
+            _log.Info("TcpSample run()");
         }
     }
 }
