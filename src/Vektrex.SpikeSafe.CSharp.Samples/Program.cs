2cdc7f6bf53b98800da45126fa98a0bcc2a9c863
Eljay Gemoto <eljay@vektrex.com>
2021-06-14 16:28:20 -0700

    Added RunBias

--- src/Vektrex.SpikeSafe.CSharp.Samples/Program.cs
+++ src/Vektrex.SpikeSafe.CSharp.Samples/Program.cs
@@ -5,6 +5,7 @@ using Vektrex.SpikeSafe.CSharp.Samples.GettingStarted.ReadAllEventsSample;
 using Vektrex.SpikeSafe.CSharp.Samples.GettingStarted.ReadIdn;
 using Vektrex.SpikeSafe.CSharp.Samples.GettingStarted.ReadMemoryTableData;
 using Vektrex.SpikeSafe.CSharp.Samples.GettingStarted.TcpSocketSample;
+using Vektrex.SpikeSafe.CSharp.Samples.RunSpikeSafeOperatingModes.RunBias;
 
 namespace Vektrex.SpikeSafe.CSharp.Samples
 {
@@ -16,19 +17,22 @@ namespace Vektrex.SpikeSafe.CSharp.Samples
             int spikeSafePortNumber = 8282;
 
             // Uncomment line below to run GettingStarted/TcpSocketSample
-            new TcpSample().Run(spikeSafeIpAddress, spikeSafePortNumber);
+            //new TcpSample().Run(spikeSafeIpAddress, spikeSafePortNumber);
             
             // Uncomment line below to run GettingStarted/ReadIdn
-            new ReadIdnExpanded().Run(spikeSafeIpAddress, spikeSafePortNumber);
+            //new ReadIdnExpanded().Run(spikeSafeIpAddress, spikeSafePortNumber);
 
             // Uncomment line below to run GettingStarted/ReadAllEvents
-            new ReadAllEventsSample().Run(spikeSafeIpAddress, spikeSafePortNumber);
+            //new ReadAllEventsSample().Run(spikeSafeIpAddress, spikeSafePortNumber);
 
             // Uncomment line below to run GettingStarted/ReadMemoryTableData
-            new ReadMemoryTableData().Run(spikeSafeIpAddress, spikeSafePortNumber);
+            //new ReadMemoryTableData().Run(spikeSafeIpAddress, spikeSafePortNumber);
+
+            // Uncomment line below to run RunSpikeSafeOperatingModes/RunBias
+            //new RunBias().Run(spikeSafeIpAddress, spikeSafePortNumber);
 
             // Uncomment line below to run MakingIntegratedVoltageMeasurements/MeasureAllPulsedVoltages
-            new MeasureAllPulsedVoltages().Run(spikeSafeIpAddress, spikeSafePortNumber);
+            //new MeasureAllPulsedVoltages().Run(spikeSafeIpAddress, spikeSafePortNumber);
         }
     }
 }
